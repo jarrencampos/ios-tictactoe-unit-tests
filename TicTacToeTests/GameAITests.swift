5af8210a614f3bf4d46bd781b6bd3b3c21a62758
Jarren Campos <jarrenmcampos@gmail.com>
2020-05-26 20:12:14 -0700

    Added Cats game test and incomplete test

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -115,8 +115,50 @@ class GameAITests: XCTestCase {
     }
     
     func testIncompleteGame() {
+        // If there is no winner
+        /*
+         o o -
+         x - o
+         o o x
+         */
+        var board = GameBoard()
+        
+        try! board.place(mark: .o, on: (0, 0))
+        try! board.place(mark: .o, on: (1, 0))
+        try! board.place(mark: .x, on: (0, 1))
+        try! board.place(mark: .o, on: (2, 1))
+        try! board.place(mark: .o, on: (0, 2))
+        try! board.place(mark: .o, on: (1, 2))
+        try! board.place(mark: .x, on: (2, 2))
+
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        
+        XCTAssertTrue(!board.isFull)
     }
 
     func testCatsGame() {
+        // If there is a tie
+        /*
+         x x o
+         o o x
+         x x o
+         */
+        var board = GameBoard()
+        
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .x, on: (1, 0))
+        try! board.place(mark: .o, on: (2, 0))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .x, on: (0, 2))
+        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (2, 2))
+
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        
+        XCTAssertTrue(board.isFull)
     }
 }
